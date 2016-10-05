56c45f55a776ae0bb5a0e06b63e96333deae82da
Georgios Fisaris <gfisaris@gmail.com>
2016-10-05 10:27:05 +0100

    Update aws-iam-access_keys.tf

--- aws-iam-access_keys.tf
+++ aws-iam-access_keys.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_access_key" "prj-user" {
+resource "aws_iam_access_key" "prj-userkey" {
 	user = "${aws_iam_user.prj-user.name}"
 	#(Required) The IAM user to associate with this access key.
 }
