6aec5af88d8c0c15aec266788a5d9661ea3aa8fa
Georgios Fisaris <gfisaris@gmail.com>
2016-10-05 10:23:09 +0100

    Update aws-iam-user_policies.tf

--- aws-iam-user_policies.tf
+++ aws-iam-user_policies.tf
@@ -1,12 +1,10 @@
-resource "aws_iam_user_policy" "prj-iam_cuplc" {
-
+resource "aws_iam_user_policy" "allow-ec2-describe-all" {
 	name = "iam-cup-ec2-${var.project_ecosystem}-${var.project_webapplication}"
 	#(Required) Name of the policy.
 	
-	user = "${aws_iam_user.prj-iam_user.name}"
+	user = "${aws_iam_user.prj-user.name}"
 	#(Required) IAM user to which to attach this policy.
-	
-	
+		
 	#policy =
 	#(Required) The policy document. This is a JSON formatted string. The heredoc syntax or file function is helpful here.
 	policy = <<EOF
