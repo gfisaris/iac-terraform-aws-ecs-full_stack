8c5b2858ba2777ef420f2b489f599e5d6dea4a7e
Georgios Fisaris <gfisaris@gmail.com>
2016-10-05 17:15:46 +0100

    Add AWS Resource: IAM Roles

---
+++ aws-iam-roles.tf
@@ -0,0 +1,30 @@
+resource "aws_iam_role" "ecs-admin" {
+    name = "iam-role-ecs-admin-${var.prj_ecosystem}-${var.prj_application}"
+    assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ecs:CreateCluster",
+        "ecs:DeregisterContainerInstance",
+        "ecs:DiscoverPollEndpoint",
+        "ecs:Poll",
+        "ecs:RegisterContainerInstance",
+        "ecs:StartTelemetrySession",
+        "ecs:Submit*",
+        "ecr:GetAuthorizationToken",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:BatchGetImage",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+
+}
