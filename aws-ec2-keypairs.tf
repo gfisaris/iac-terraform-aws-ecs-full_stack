f2dbaec317e2e95ec055e7cf13059ad48014fa76
Georgios Fisaris <gfisaris@gmail.com>
2016-10-05 11:24:22 +0100

    Update aws-ec2-keypairs.tf

--- aws-ec2-keypairs.tf
+++ aws-ec2-keypairs.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "prj-user_keypair" {
-  key_name    = "ec2-kp-${var.prj-ecosystem}-${var.prj-application}"
+  key_name    = "ec2-kp-${var.prj_ecosystem}-${var.prj_application}"
   public_key  = "${var.ec2_keypair_public}"
 }
