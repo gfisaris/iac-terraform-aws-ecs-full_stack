5e7a1c0a55349da458602642f2c07609657f6988
Georgios Fisaris <gfisaris@gmail.com>
2016-10-05 11:12:16 +0100

    Update aws-sns-topics.tf

--- aws-sns-topics.tf
+++ aws-sns-topics.tf
@@ -1,14 +1,13 @@
-resource "aws_sns_topic" "prj-sns_topic-ec2_autoscalling_notification" {
+resource "aws_sns_topic" "asg_notifications" {
 
-	name = "sns-tpc-ec2_asg-${var.project_ecosystem}-${var.project_webapplication}-${var.project_environment}"
+	name = "sns-tpc-ec2_asg-${var.prj_ecosystem}-${var.prj_application}-${var.prj_environment}"
 	#(Required) The friendly name for the SNS topic
 	#Up to 256 alphanumeric characters, hyphens (-), and underscores (_) are allowed.
 	
-	display_name = "${var.project_ecosystem}-${var.project_webapplication}-${var.project_environment}"
+	display_name = "${var.prj_ecosystem}-${var.prj_application}-${var.prj_environment}"
 	#(Optional) The display name for the SNS topic
 	#Required for SMS subscriptions (can be up to 10 characters). Optional for other transports.
 
-	
 	#policy = ""
 	#(Optional) The fully-formed AWS policy as JSON
 	
